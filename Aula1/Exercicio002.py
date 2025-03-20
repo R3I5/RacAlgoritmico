0ddf564b45993bcd68f54fcb9b30269d7041f3e5
João Victor dos Reis da Silva <j.victor.reis20044@gmail.com>
2025-03-20 09:45:54 -0300

    Aula 1 RacAlg

---
+++ Aula1/Exercicio002.py
@@ -0,0 +1,2 @@
+idade = int(input("Digite a sua idade:"))
+print("Sua idade é:",idade)
---
+++ Aula1/Exercicio003.py
@@ -0,0 +1,7 @@
+numero1 = int(input("Digite o primeiro número:"))
+numero2 = int(input("Digite o segundo número:"))
+soma = numero1+numero2
+sub = numero1-numero2
+mult = numero1*numero2
+div = numero1/numero2
+print("O resultado da soma é:",soma,",da subtração:",sub,", da multiplicação:",mult,", da divisão:",div)
