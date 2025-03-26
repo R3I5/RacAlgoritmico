4abfc1ba7099d542c90cf64e34e8ed0acd65c68f
João Victor dos Reis da Silva <j.victor.reis20044@gmail.com>
2025-03-26 10:29:07 -0300

    Exercicio 010

---
+++ Aula1/Exercicio010.py
@@ -0,0 +1,7 @@
+# Dado um retângulo de lado a=7 e área 63, escreva:
+# Uma instrução em Python que calcule o valor do B
+
+ladoA = 7
+area = 63
+ladoB = area/ladoA
+print(f"O valor do lado B é:{ladoB}")
