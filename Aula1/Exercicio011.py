64f4d01c8be90786a7ebbb33511887faa19f5b64
João Victor dos Reis da Silva <j.victor.reis20044@gmail.com>
2025-03-26 10:41:30 -0300

    Exercicio 011

---
+++ Aula1/Exercicio011.py
@@ -0,0 +1,11 @@
+# Dado um retângulo de lado a=7 e área 63, escreva:
+# Uma instrução em Python que calcule o valor do B
+
+import math
+
+ladoA = 7
+area = 63
+ladoB = area/ladoA
+print(f"O valor do lado B é:{ladoB}")
+d = math.sqrt((ladoA**2)+(ladoB**2))
+print(f"O valor da diagonal é:{d:.2f}")
