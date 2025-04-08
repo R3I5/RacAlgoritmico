50688b0686f22d5dbbe0b51b39127722af559f9e
João Victor dos Reis da Silva <j.victor.reis20044@gmail.com>
2025-04-08 11:23:27 -0300

    Exercicio 1 versão 2

---
+++ Aula6/Exercicio01_ver2.py
@@ -0,0 +1,14 @@
+"""
+Escreva um programa que imprima os números pares de 1 a 20 
+utilizando um laço de repetição
+
+x = 0
+while (x<20):
+    x=x+2
+    print(x)
+"""
+for i in range(0,20,2):
+    i = i+2
+    print(i)
+
+
