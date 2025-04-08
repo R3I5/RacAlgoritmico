9c422134f0d114bc41f7609a8297dc4c83ad68fb
João Victor dos Reis da Silva <j.victor.reis20044@gmail.com>
2025-04-08 11:51:12 -0300

    Exericio 4

---
+++ Exercicio04.py
@@ -0,0 +1,6 @@
+"""
+Escreva um programa que imprima as os números de 100 a 1 em ordem decrescente utilizando um laço de repetição for e a função range7
+"""
+
+for i in range(100,0,-1):
+    print(i)
