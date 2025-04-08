ef395f84d4f72ce6942acf7a8326cdd70dfb7286
João Victor dos Reis da Silva <j.victor.reis20044@gmail.com>
2025-04-08 10:59:28 -0300

    Exercicio 2

---
+++ Aula6/Exercicio02.py
@@ -0,0 +1,10 @@
+"""
+Escreva um programa que calcula a soma dos números de 1 a 100
+utilizando um laço de repetição for e a funlçao range
+"""
+
+x = 0
+for i in range(100):
+    i = i+1
+    x = i + x
+    print(x)
