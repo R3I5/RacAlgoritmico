768fa658682648f421b0f0470f407bcf0e28dbfd
João Victor dos Reis da Silva <j.victor.reis20044@gmail.com>
2025-04-08 11:28:16 -0300

    Exercicio 3 versão 2

---
+++ Aula6/Exercicio03_ver2.py
@@ -0,0 +1,15 @@
+"""
+Escreva um programa que imprima os números de 1 a 10, cada um
+elevado ao quadrado, utilizando um laço de repetição
+
+x = 0
+while (x < 10):
+    x = x + 1
+    y = x**2
+    print(y)
+"""
+x = 0
+for i in range(0,10):
+    i = i + 1
+    x = i**2
+    print(x)
