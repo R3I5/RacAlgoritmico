6f5bac68e19368ec334907eeb4e954423dff11d6
João Victor dos Reis da Silva <j.victor.reis20044@gmail.com>
2025-04-09 10:49:40 -0300

    Update Exercicio 9

--- Aula6/Exercicio09.py
+++ Aula6/Exercicio09.py
@@ -1,14 +1,19 @@
 """
 Escreva um programa que imprima os numeros primos de 1 a 100 utilizando um laço de repetição for e range
 """
+# Lista para armazenar os números primos
+primos = []
 
-for i in range(1,101):
-    if i<2:
-        continue
-    eh_primo = True
-    for j in range(2,1):
-        if i % j == 0:
-            eh_primo = False
-        break
-    if eh_primo:
-        print(i)
+# Loop de 2 a 100
+for num in range(2, 101):
+    primo = True
+    for i in range(2, num):
+        if num % i == 0:
+            primo = False
+            break
+    if primo:
+        primos.append(num)
+
+# Imprime a lista de números primos
+print("Números primos de 1 a 100:")
+print(primos)
