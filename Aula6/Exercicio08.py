a1a9c947fc6458e25a6e0e823b60ba6f745e9a78
João Victor dos Reis da Silva <j.victor.reis20044@gmail.com>
2025-04-09 10:09:00 -0300

    Update Exercicio 7

--- Aula6/Exercicio08.py
+++ Aula6/Exercicio08.py
@@ -1,10 +1,4 @@
 """
-Escreva um programa que solicite ao usuario um numero e imprima a tabuada deste numero de 1 a 10, utilizando um laço de repetição for
+Escreva um programa que calcule a média de 5 números inseridos pelo usuario, utilizando um laço de repetição while
 """
 
-x = int(input('Digite um número:'))
-y = 0
-for i in range(1,11):
-    y = x * i
-    print(y)
-    
