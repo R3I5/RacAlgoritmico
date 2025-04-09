66223f5b0168126d1b89da279005e6f5bf0cfee7
João Victor dos Reis da Silva <j.victor.reis20044@gmail.com>
2025-04-09 10:18:49 -0300

    Create Exercicio 8

--- Aula6/Exercicio08.py
+++ Aula6/Exercicio08.py
@@ -2,3 +2,12 @@
 Escreva um programa que calcule a média de 5 números inseridos pelo usuario, utilizando um laço de repetição while
 """
 
+x = 0
+soma = 0
+while(x<5):
+    x = x + 1
+    valor = float(input('Digite 5 números: '))
+    soma += valor
+    
+media = soma / 5
+print(media)
