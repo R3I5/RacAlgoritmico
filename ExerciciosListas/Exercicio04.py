b5b2cbd99bcf0cc3980a3f014cb5b2ce5e2d554c
João Victor dos Reis da Silva <j.victor.reis20044@gmail.com>
2025-05-08 10:38:46 -0300

    Update Exercicio 04 Lista sobre Listas

--- ExerciciosListas/Exercicio04.py
+++ ExerciciosListas/Exercicio04.py
@@ -1,8 +1,16 @@
 # Faça um programa que leia um vetor de 10 caracteres, e diga quantas consoantes foram lidas. Imprima as consoantes.
 
-vetor = []
-vogais = [a, e, i, o, u]
+letras = []
+vogais = ['a', 'e', 'i', 'o', 'u']
+consoantes = []
+qtdConsoantes = 0
+
 for i in range(10):
-    caracteres = input(f'Digite o {i+1} caracter: ')
-    vetor.append(caracteres)
-print(vetor)
+    caracteres = input(f'Digite o {i+1}° caracter: ')
+    letras.append(caracteres)
+    if caracteres not in vogais:
+        consoantes.append(caracteres)
+        qtdConsoantes = qtdConsoantes + 1
+print(f'As letras são: {letras}')
+print(f'Foram digitadas {qtdConsoantes} consoantes')
+print(f'As consoantes digitadas são: {consoantes}')
