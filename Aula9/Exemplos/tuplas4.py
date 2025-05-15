09c81d796867fd62557849437549823535cfa736
João Victor dos Reis da Silva <j.victor.reis20044@gmail.com>
2025-05-15 09:06:44 -0300

    Exemplo 4 Aula 9

---
+++ Aula9/Exemplos/tuplas4.py
@@ -0,0 +1,9 @@
+# Elaborar um aplicativo que demonstre a necessidade de finalizar uma tupla de um único elemento com uma vírgula no final
+
+tupla1 = ("Brasil")
+print(type(tupla1))
+tupla2 = ("Brasil",)
+print(type(tupla2))
+
+# a presença da virgula em tupla2 transforma a variavel em uma tupla
+
