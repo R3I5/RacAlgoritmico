bf5fa9b56ae9823379ffc3ab21bea8248d7283be
João Victor dos Reis da Silva <j.victor.reis20044@gmail.com>
2025-05-19 10:12:40 -0300

    Exercicio 3 Tuplas

---
+++ Aula9/Exercicios/Exercicio03.py
@@ -0,0 +1,7 @@
+# Criar um programa que cadastre locais históricos do mundo com suas coordenadas fazendo uso de tuplas com parâmetros nomeados. Dica: usar a função namedtuple()
+
+localHistorico = ()
+print(" Cadastro de locais históricos ")
+while True:
+    nome = input("Digite o nome do local histórico: ")
+    
