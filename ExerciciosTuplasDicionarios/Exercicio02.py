4ca0eb4c4466fd7fe85fc953679c003508ca6998
João Victor dos Reis da Silva <j.victor.reis20044@gmail.com>
2025-05-20 10:47:51 -0300

    Exercicio Frota Alfa

---
+++ ExerciciosTuplasDicionarios/Exercicio02.py
@@ -0,0 +1,22 @@
+# 2. Frota Alfa
+
+ano_luz = {
+    "pc": 0.31,
+    "al": 1,
+    "ae": 63241.09,
+    "ml": 525960.23,
+    "sl": 31557609.92
+}
+unidades = [
+    "Parsec (pc)", 
+    "Ano-Luz (al)", 
+    "Unidade Astronômica (ae)",
+    "Minuto-Luz(ml)", 
+    "Segundo-Luz (sl)"]
+
+# Imprimir a lista de unidades de conversão;
+while True:
+    print("Iniciando conversão de unidades...")
+    for unidade in unidades:
+        print(unidade)
+    break
