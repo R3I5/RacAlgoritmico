853e89c0722832603eb1599aea0c7fc9aa6c5b14
João Victor dos Reis da Silva <j.victor.reis20044@gmail.com>
2025-05-21 10:18:29 -0300

    Exemplo funções

--- Aula10/Exemplos/funcoes1.py
+++ Aula10/Exemplos/funcoes1.py
@@ -1,1 +1,14 @@
-# teste
+# Funções são fragmentos de códigos que permitem organizar algoritmos e reduzir sua repetição.
+# A partir do uso correto de funções um projeto fica mais fácil de manter, bem como torna-se mais legível, uma vez que funcionalidades específcas podem ser colocadas em poucas linhas
+# Exemplo:
+
+def soma(num1, num2):
+    s = num1 + num2
+    return s
+
+numero1 = int(input("Digite o primeiro numero: "))
+numero2 = int(input("Digite o segundo numero: "))
+
+resultado = soma(numero1,numero2)
+
+print(f"O resultado é: {resultado}")
