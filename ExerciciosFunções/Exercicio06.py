753b2d3a7c2f8baf6562981c0c4bce95f4cc56ce
João Victor dos Reis da Silva <j.victor.reis20044@gmail.com>
2025-05-28 10:14:16 -0300

    Exercicio 06 Funções

--- ExerciciosFunções/Exercicio06.py
+++ ExerciciosFunções/Exercicio06.py
@@ -3,3 +3,7 @@
 # uma para fazer a conversão e uma para a saída. Registre a informação AM/PM como um valor 'a' para am e 'p' para PM. 
 # Assim, a função para efetuar as conversões tera um parâmetro formal para registrar se é AM ou PM. Inclua um loop que permita que o usuario repita esse cálculo para novos valores de entrada todas as vezes que desejar. 
 
+
+def conversao(horas24, minutos24):
+    if horas24<12:
+        return f'{horas24:0d}'
