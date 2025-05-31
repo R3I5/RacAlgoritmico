74f2e18b6887da98ed281657847c7147b919963d
João Victor dos Reis da Silva <j.victor.reis20044@gmail.com>
2025-05-31 18:19:00 -0300

    Lista Para Dicionario

---
+++ RevisãoProva2/Estudos/3.ListaParaDicionario.py
@@ -0,0 +1,7 @@
+"""
+3. Funções e conversão de tipos: lista para Dicionario
+Escreva uma função chamada listaParaDicionario(chaves, valores) que receba duas listas como entrada: chaves e valores
+- A função deve criar e retornar um dicionario onde cada elemento da lista chaves é uma chave, e o elemento correspondente na lista valores(mesmo indice) é o seu valor
+- Considere que as listas chaves e valores terão sempre o mesmo tamanho
+- Se a lista de chaves contiver elementos duplicados, o ultimo valor associado a essa chave na lsita valores deve prevalecer
+"""
